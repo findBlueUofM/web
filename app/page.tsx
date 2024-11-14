a21b0c82511e28a6ff997aed68135da6f8bdb82a
PraveshKunwar <praveshk04@outlook.com>
2024-11-14 18:00:05 -0500

    feat: some boilerplate code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { useRouter } from "next/navigation";
 import Button from '../components/Button'
@@ -10,7 +12,7 @@ export default function Home() {
 }
   return (
     <div>
-      
+        Hello world!
     </div>
   );
 }
