dc2b868e6172ef49336f10e46eb2e59a73440ab6
ojlennon <45679125+ojlennon@users.noreply.github.com>
2024-12-03 13:03:17 -0500

    fixed dyanmic routing

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -5,6 +5,7 @@ import { use, useState, useEffect } from "react"; // Import use
 import { supabase } from "@/lib/supabase";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { useRouter } from "next/router";
 import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
 interface PostQuery {
@@ -21,17 +22,17 @@ interface PostQuery {
 //   repo: Repo
 // }>
 
-export default function UniquePosts({ params }: { params: { id: string } }) {
-  // @ts-expect-error
-  const resolvedParams: PostQuery = use(params); // Unwrap the promise
+export default function UniquePosts(props) {
+  const router = useRouter();
+  // const resolvedParams: PostQuery = use(params); // Unwrap the promise
   const [post, setPost] = useState({}); //post is a json with the data from supabase
-
+  const { id } = router.query;
   useEffect(() => {
     const retrievePost = async () => {
       const { data, error } = await supabase
         .from("Posts")
         .select("*")
-          .eq("id", resolvedParams.id);
+          .eq("id", id);
         
         if (error) {
             
@@ -46,7 +47,7 @@ export default function UniquePosts({ params }: { params: { id: string } }) {
   return (
     <div className="uniquePost">
       <Navbar />
-      <p>Post: {resolvedParams.id}</p>
+      <p>Post: {id}</p>
       <Footer />
     </div>
   );
